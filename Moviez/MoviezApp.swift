a79a5d27410d62bfd572c0c0c1682bedbd276924
Manuel Martinez <anymus89hd@gmail.com>
2022-07-31 17:16:56 -0500

    Initial Commit

---
+++ Moviez/MoviezApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviezApp.swift
+//  Moviez
+//
+//  Created by Manuel Martinez on 31/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MoviezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
